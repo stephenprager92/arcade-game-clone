11ac433d29bbef529e84c3d4a1fa07943b51591a
Steve Prager <stephenprager92@gmail.com>
2018-06-26 00:30:42 -0400

    Added light markup (instructions & score)

--- js/engine.js
+++ js/engine.js
@@ -26,7 +26,8 @@ var Engine = (function(global) {
 
     canvas.width = 505;
     canvas.height = 606;
-    doc.body.appendChild(canvas);
+    const gameCanvas = doc.querySelector('.gameCanvas');
+    gameCanvas.appendChild(canvas);
 
     /* This function serves as the kickoff point for the game loop itself
      * and handles properly calling the update and render methods.
